7d9f0a87571c02f0d353750762d9669b29483b83
Jan Jakubuv <jakubuv@gmail.com>
2022-11-02 22:25:47 +0100

    first draft

--- new/docker-update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export JEKYLL_VERSION=3.8
-docker run --rm --volume="$PWD:/srv/jekyll:Z" --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" -it jekyll/builder:$JEKYLL_VERSION bundle update
-
